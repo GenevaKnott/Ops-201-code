bca0797140788129753ac569167f2b395eb4a709
GenevaKnott <geneva.vitales@yahoo.com>
2023-02-08 23:11:54 +0000

    change

---
+++ ops-201-challenge-03.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author:Geneva Knott
+# Date of latest revision:02082023
+# Purpose: Functions
+
++ Write a script that prints the login history of users on this computer.
++ Your script must use at least one function and one variable.
+
+# Main
+
+# End
