3af40985350f38f27c7c58ca834502b78a847a1f
GenevaKnott <geneva.vitales@yahoo.com>
2023-02-15 22:45:55 +0000

    update

---
+++ ops-201-challenge-08.bat
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Geneva Knott
+#15 Feb 23
+
+# I worked with Justin H. and Sierra the M-N-M team!
+
+# Main
+@echo off
+ROBOCOPY C:\Users\j.thompson\Desktop C:\Users\j.thompson\documents
+/LOG:backuplog.txt
+pause
+
+# End 
