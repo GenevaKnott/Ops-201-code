d1f06beeb1e3776485ed134edaa93843d1808162
GenevaKnott <geneva.vitales@yahoo.com>
2023-02-16 22:36:38 +0000

    code challenge

---
+++ ops-201-challenge-09.ps1
@@ -0,0 +1,32 @@
+<!---
+# Script Name ops-201-challenge-09.ps1
+# Author:Geneva Knott
+# Date: 16 Feb 2023
+# Description see below
+
+$begin = get-date -date "02/16/2023 08:00:00"
+$End = Get-date -date "02/16/2023 14:00:00"
+
+# Main
+
+#Output all events in 24 to last_24.txt
+
+Get-eventlog -logname system -after $begin -before $end | Outfile C:\test\last_24.txt
+
+#export error codes in a file named error.txt
+
+Get-eventlog -logname system -entrytype error | outfile C:\test\error.txt
+
+#Print to the screen all events with ID of 16
+
+Get-eventlog -logname system -InstanceID 16
+
+#Print to the screen the most recent 20 entries
+
+Get-eventlog -logname system -newest 20
+
+#Print to the screen the most recent 500 entries
+
+Get-eventlog -logname system -newest 500
+
+#end
