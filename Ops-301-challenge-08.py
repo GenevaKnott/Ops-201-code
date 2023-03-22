e2279acb0f495f4ce7a0e4688910c33a4a7ecc28
GenevaKnott <geneva.vitales@yahoo.com>
2023-03-22 21:54:07 +0000

    code

---
+++ Ops-301-challenge-08.py
@@ -0,0 +1,27 @@
+#!/user/bin/env python3
+# Geneva Knott - worked with Justin H, Sierra M & Nick A
+# Python
+#22 Mar 2023
+
+
+# main
+
+# Define the list
+
+Superlist = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+
+#print fourth element of list
+
+print(Superlist[3])
+
+#print the sixth through tenth element
+
+print(Superlist[5:9])
+
+# Change the 7th element to onion
+
+Superlist[6]=("cant count")
+
+print(Superlist)
+
+# end
