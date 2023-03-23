0b8fa6430db0b424947ed33dd3bb45d0e372c33f
GenevaKnott <geneva.vitales@yahoo.com>
2023-03-23 17:28:59 +0000

    code

---
+++ Ops-301-challenge-09.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Geneva Knott- worked with Justin H, Sierra & Nick A
+# Logic
+#23Mar23
+
+# main
+
+
+choiceA = input("Choice a number for choice A!")
+choiceB = input("Choice a number for choice B!")
+choiceC = input("Choice a number for choice C!")
+choiceD = input("Choice a number for choice D!")
+
+
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("A and B are equal")
+else:
+  print("A is greater than B")
+if c <= d:
+    print ("C is lesser than /equal to D")
+else:
+    print ("C is greater than / equal to D")
+if d != c:
+    print ("C is not equal to D")
+
+#end
